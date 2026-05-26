4eee38ff613e82a6a2f6e9de966a4163b6b4993e
Nagulan24 <nagulan1107@gmail.com>
2026-05-26 22:42:49 +0530

    list problem

--- problems.py
+++ problems.py
@@ -13,3 +13,11 @@ for ch in text:
    if ch not in checked:
       print(ch,text.count(ch))
       checked += ch
+
+#find the largest number in a list
+numbers = [10, 2, 9, 1, 3]
+lar=numbers[0]
+for i in numbers:
+   if i > lar:
+      lar=i
+print("largest number is:",lar)
