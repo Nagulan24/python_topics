c3d159106a24e55bc01802cca572afa0877caa01
Nagulan24 <nagulan1107@gmail.com>
2026-05-27 18:45:52 +0530

    if else problem

--- problems.py
+++ problems.py
@@ -1,23 +1,45 @@
-#palindrome
+''' #palindrome
 text =input("enter the txt:").lower().replace(" ","")
 if text == text[::-1]:
   print("palidrome")
 else:
-    print("not a palindrome")  
+    print("not a palindrome")  '''
 
 
-#character frequency counter
+'''#character frequency counter
 text = input("enter the txt:")
 checked=""
 for ch in text:
    if ch not in checked:
       print(ch,text.count(ch))
-      checked += ch
+      checked += ch '''
 
-#find the largest number in a list
+'''#find the largest number in a list
 numbers = [10, 2, 9, 1, 3]
 lar=numbers[0]
 for i in numbers:
    if i > lar:
       lar=i
-print("largest number is:",lar)
+print("largest number is:",lar)'''
+
+
+'''#odd and even number
+num = int(input("enter a num"))
+if num % 2 == 0:
+   print("even")
+else:
+   print("odd")'''
+
+#ATM Withdrawal Validation
+bal = 10000
+withdraw = int (input("enter amount:\t"))
+if withdraw > 0:
+   if  withdraw % 100 == 0:
+        if withdraw <= bal:
+         print("withdrawal successful")
+        else :
+           print("insufficient balance")
+   else:
+      print("enter amoumt mutiple of 100")
+else:  
+   print("enter a valid amount")
